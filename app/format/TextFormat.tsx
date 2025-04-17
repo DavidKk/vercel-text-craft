db62c6771c8cdfe33c7933b8de693784cf2d5c7f
DavidJones <qowera@gmail.com>
2025-04-18 00:16:13 +0800

    fix: style

--- app/format/TextFormat.tsx
+++ app/format/TextFormat.tsx
@@ -35,13 +35,13 @@ export default function TextFormat() {
         <FormatTabs value={targetFormat} onChange={setTargetFormat} types={FINDER_FORMAT_TYPES} />
       </div>
 
-      <div className="flex flex-col md:flex-row gap-1 w-full md:min-h-[500px] md:h-[60vh]">
+      <div className="flex flex-col md:flex-row gap-1 w-full md:min-h-[500px] md:h-[64vh]">
         <div className="w-full md:w-1/2 min-h-[250px] h-full">
-          <ReactEditor title="Source" className="min-h-[70vh] md:min-h-[100%]" value={sourceText} onChange={setSourceText} storageKey="text-format-source" />
+          <ReactEditor title="Source" className="min-h-[64vh] md:min-h-[100%]" value={sourceText} onChange={setSourceText} storageKey="text-format-source" />
         </div>
 
         <div className="w-full md:w-1/2 min-h-[250px] h-full">
-          <ReactEditor title="Formatted" className="min-h-[70vh] md:min-h-[100%]" value={formattedText} />
+          <ReactEditor title="Formatted" className="min-h-[64vh] md:min-h-[100%]" value={formattedText} />
         </div>
       </div>
     </div>
